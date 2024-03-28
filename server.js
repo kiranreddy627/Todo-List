f1b1a39a41847fd7a643deb3daaf5c2d52022b62
kiranreddy627 <omkiranreddybojja7@gmail.com>
2024-03-28 18:52:24 +0530

    Todo List Completed

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const TaskSchema = require('./model')
+const cors = require('cors');
+
+
+const app = express();
+app.use(express.json());
+
+app.use(cors({
+    origin: '*'
+}))
+mongoose.connect('mongodb+srv://kiran:kiran@todolist.von05st.mongodb.net/?retryWrites=true&w=majority&appName=todolist').then(() => console.log('DB connected'));
+
+
+app.post('/addtask',async(req,res)=>{
+    const {todo} = req.body;
+    try{
+        const newData = new TaskSchema({
+            todo : todo
+        });
+        await newData.save();
+        return res.json(await TaskSchema.find())
+    }
+    catch(err){
+        console.log(err)
+    }
+})
+
+app.get('/gettask', async (req, res) => {
+    try {
+        return res.json(await TaskSchema.find());
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+app.delete('/delete/:id', async (req, res) => {
+    try {
+        await TaskSchema.findByIdAndDelete(req.params.id)
+        return res.json(await TaskSchema.find())
+    } catch (error) {
+        console.log(error)
+    }
+})
+app.listen(5000,()=> console.log("server is running...."))
